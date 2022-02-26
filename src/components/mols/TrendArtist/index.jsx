0e450514a89b29f410cd75a9b6e251d0c949c853
ThalisDev <thalis224@hotmail.com>
2022-02-26 19:57:49 -0300

    feat: :sparkles: criando trend artist

--- src/components/mols/TrendArtist/index.jsx
+++ src/components/mols/TrendArtist/index.jsx
@@ -8,6 +8,8 @@ import {
 	TrendNftArea,
 	TrendArtistContainer,
 	TicketArtistArea,
+	TicketLeilaoArea,
+	ButtonArtwork,
 } from "./styles.js";
 import TicketArtistName from "../TicketArtistName/index.jsx";
 
@@ -24,6 +26,10 @@ const TrendArtist = () => {
 				<TicketArtistArea>
 					<TicketArtistName titleArtist="@leo" createBy="Create By" />
 				</TicketArtistArea>
+				<TicketLeilaoArea>
+					<h3>auction ends</h3>
+					<ButtonArtwork>View Artwork</ButtonArtwork>
+				</TicketLeilaoArea>
 			</Header>
 		</TrendArtistContainer>
 	);
