dbfd00c19817b8709dbfba9919a6887f0c260a3e
Gabriel4420 <gabriel_rodrigues_perez@hotmail.com>
2022-03-02 00:17:25 -0300

    fix: resolvendo alguns pontos de responsividade

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,40 +1,43 @@
-import React from "react";
+import React from 'react'
+import '@fontsource/playfair-display'
+import Navbar from '../../components/mols/NavBar'
+import Footer from '../../components/template/footer'
+import TrendArtist from '../../components/mols/TrendArtist'
 
-/* Custom style*/
-import "@fontsource/playfair-display";
-import Navbar from "../../components/mols/NavBar";
-import Footer from "../../components/template/footer";
-import TrendArtist from "../../components/mols/TrendArtist";
-import TicketArtistName from "../../components/mols/TicketArtistName";
-import "bootstrap/dist/css/bootstrap.css";
-import { Col, Row } from "reactstrap";
-import { TropicalTokenArea } from "./styles";
+import { Col, Row } from 'reactstrap'
+import { AboutArea } from './styles'
 
 const Home = () => {
-	return (
-		<>
-			<Navbar />
-			<TrendArtist />
-			<TropicalTokenArea>
-				<Row xs="2">
-					<Col>
-						<img src="/images/logo_nav.svg" alt="Logo" style={{ width: 300 }} />
-					</Col>
-					<Col>
-						<h2>What is a tropical token?</h2>
-						<p>
-							Tropical Tokens is the backbone of content development between
-							creators, digitality, and supporters of all kinds.
-							Digital-tropicality is multiplicity, synergy and abundance. It’s
-							feasibility.
-						</p>
-						<h4>know more</h4>
-					</Col>
-				</Row>
-			</TropicalTokenArea>
-			<Footer />
-		</>
-	);
-};
+  return (
+    <>
+      <Navbar />
+      <TrendArtist />
+      <AboutArea>
+        <Row sm="2">
+          <Col>
+            <img src="/images/logo_nav.svg" alt="Logo" style={{ width: 300 }} />
+          </Col>
+          <Col>
+            <h2>What is a tropical token?</h2>
+            <p>
+              Tropical Tokens is the backbone of content development between
+              creators, digitality, and supporters of all kinds.
+              Digital-tropicality is multiplicity, synergy and abundance. It’s
+              feasibility.
+            </p>
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://tropicaltokens.com/#start"
+            >
+              know more
+            </a>
+          </Col>
+        </Row>
+      </AboutArea>
+      <Footer />
+    </>
+  )
+}
 
-export default Home;
+export default Home
--- src/pages/home/styles.js
+++ src/pages/home/styles.js
@@ -1,6 +1,19 @@
-import styled from "styled-components";
+import styled from 'styled-components'
 
-export const TropicalTokenArea = styled.div`
-	padding: 20px;
-	text-align: center;
-`;
+export const AboutArea = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  padding: 80px;
+  a {
+    text-decoration: none;
+    color: black;
+    font-size: 30px;
+    padding-left: 5px;
+  }
+
+  a:hover {
+    text-decoration: underline;
+  }
+`
