12cdc12e3018c2f0ba45bfc07a7d720dfc6d98dc
Gabriel4420 <gabriel_rodrigues_perez@hotmail.com>
2022-03-05 18:58:03 -0300

    fix: removendo swipper

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -6,25 +6,13 @@ import TrendArtist from '../../components/mols/TrendArtist'
 import { Link } from 'react-router-dom'
 import { Col, Row } from 'reactstrap'
 import { AboutArea } from './styles'
-import {Swiper,SwiperSlide} from 'swiper/react'
-import 'swiper/swiper.scss'
+
 const Home = () => {
   return (
     <>
       <Navbar isMobile />
       <TrendArtist />
-      <Swiper
-        spaceBetween={50}
-        slidesPerView={3}
-        onSlideChange={() => console.log('slide change')}
-        onSwiper={(swiper) => console.log(swiper)}
-      >
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        ...
-      </Swiper>
+      
       <AboutArea>
         <div style={{ paddingRight: '50px' }}>
           <img
